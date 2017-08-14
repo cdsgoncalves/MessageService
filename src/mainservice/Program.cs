01d8fc7b99307303e913bab1551ee8067ae21d33
Murilo Beltrame <murilobeltrame@gmail.com>
2017-08-14 00:25:08 -0300

    New recipe for just ELK, Serilog Main App Loggin, Main default port to 80

--- src/mainservice/Program.cs
+++ src/mainservice/Program.cs
@@ -17,6 +17,7 @@ namespace mainservice
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
                 .UseStartup<Startup>()
+                .UseUrls("http://localhost:80")
                 .Build();
 
             host.Run();
--- src/mainservice/Startup.cs
+++ src/mainservice/Startup.cs
@@ -9,6 +9,9 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.PlatformAbstractions;
+using Serilog;
+using Serilog.Events;
+using Serilog.Sinks.Elasticsearch;
 using Swashbuckle.AspNetCore.Swagger;
 
 namespace mainservice
@@ -23,6 +26,12 @@ namespace mainservice
                 .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
                 .AddEnvironmentVariables();
             Configuration = builder.Build();
+
+            // Create Serilog Elasticsearch and Console logger
+            var logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(Configuration)
+                .Enrich.FromLogContext();
+            Log.Logger = logger.CreateLogger();
         }
 
         public IConfigurationRoot Configuration { get; }
@@ -32,23 +41,23 @@ namespace mainservice
         {
             // Add framework services.
             services.AddMvc();
-
             // Add logging service. 
-            // TODO: Write all exceptions to a collection into ES
             services.AddLogging();
 
             // Register the Swagger generator
-            services.AddSwaggerGen(g=>{
-                g.SwaggerDoc("v1", new Info{ 
-                    Title="Message Service API", 
-                    Version="v1",
-                    Description="The Message Service is, as name says, a Service to manage submissions of messages of a bunch of kinds in a SOA/Microservices cenario. It`s intended to be the gateway to send and track submissions of email, sms, push notifications and so on.",
-                    Contact=new Contact{Name="XOPZ team", Url="https://github.com/xopz"},
-                    License= new License{Name="Use under MIT", Url="https://github.com/xopz/MessageService/blob/master/LICENSE.txt"}
+            services.AddSwaggerGen(g =>
+            {
+                g.SwaggerDoc("v1", new Info
+                {
+                    Title = "Message Service API",
+                    Version = "v1",
+                    Description = "The Message Service is, as name says, a Service to manage submissions of messages of a bunch of kinds in a SOA/Microservices cenario. It`s intended to be the gateway to send and track submissions of email, sms, push notifications and so on.",
+                    Contact = new Contact { Name = "XOPZ team", Url = "https://github.com/xopz" },
+                    License = new License { Name = "Use under MIT", Url = "https://github.com/xopz/MessageService/blob/master/LICENSE.txt" }
                 });
                 //Set the comments path for the swagger json and ui.
                 var basePath = PlatformServices.Default.Application.ApplicationBasePath;
-                var xmlPath = Path.Combine(basePath, "mainservice.xml"); 
+                var xmlPath = Path.Combine(basePath, "mainservice.xml");
                 g.IncludeXmlComments(xmlPath);
             });
         }
@@ -56,8 +65,16 @@ namespace mainservice
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
-            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
-            loggerFactory.AddDebug();
+            loggerFactory
+                .AddDebug()
+                // Add serilog
+                // The Sinks are configured to write both ES and Console
+                .AddSerilog();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
 
             app.UseMvc();
 
